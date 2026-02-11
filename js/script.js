93fb73fe0e4f79061f9dcb567163287a1204e297
coderku <kkokku99@gmail.com>
2026-02-11 15:08:49 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// submenu
+$(function () {
+    $(".submenu").hide()
+    $(".gnb li a").mouseenter(function () {
+        $(".submenu").stop().slideDown()
+    });
+    $(".nav").mouseleave(function () {
+        $(".submenu").stop().slideUp()
+    });
+    // top버튼을 누르면,
+    $(".top_btn").click(function () {
+        // 문서 전체가 움직인다.
+        $("body,html").animate({
+            scrollTop: '0'
+        }, 500);
+    });
+    // top버튼을 안보이게 한다
+    $(".top_btn").hide();
+    // 브라우저 창에서 스크롤하면,
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            //top버튼이 보인다.
+            $(".top_btn").fadeIn()
+        } else {
+            // top버튼이 안보인다.
+            $(".top_btn").fadeOut()
+        }
+    });
+    $(".answer").hide();
+    $(".qna_btn").click(function () {
+        const nextAns = $(this).next(".answer");
+        $(".answer").not(nextAns).slideUp();
+        nextAns.slideToggle();
+        $(this).children("strong").toggleClass("color");
+        $(".qna_btn").not(this).children("strong").removeClass("color");
+        $(this).children("span").toggleClass("turn");
+        $(".qna_btn").not(this).children("span").removeClass("turn");
+    });
+});
